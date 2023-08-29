8a6f21325fe62454b882df41736eafdc45e8bbfd
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-29 19:02:07 +0100

    implement join mission action

--- src/redux/mission/missionSlice.js
+++ src/redux/mission/missionSlice.js
@@ -12,13 +12,28 @@ export const fetchMission = createAsyncThunk(
   async () => {
     try {
       const response = await axios.get(
-        'https://api.spacexdata.com/v3/missions',
+        'https://api.spacexdata.com/v3/missions'
       );
       return response.data;
     } catch (error) {
-      return error;
+      return error.message;
     }
-  },
+  }
+);
+
+export const joinMission = createAsyncThunk(
+  'joinMission',
+  async (missionId) => {
+    try {
+      const response = await axios.get(
+        `https://api.spacexdata.com/v3/missions/${missionId}`,
+        { reserved: true }
+      );
+      return response.data.mission_id;
+    } catch (error) {
+      return error.message;
+    }
+  }
 );
 
 const missionSlice = createSlice({
@@ -35,6 +50,18 @@ const missionSlice = createSlice({
     });
     builder.addCase(fetchMission.rejected, (state, action) => {
       state.loading = false;
+      state.error = action.error;
+    });
+    builder.addCase(joinMission.fulfilled, (state, action) => {
+      const missionId = action.payload;
+      state.mission = state.mission.map((mission) =>
+        mission.mission_id !== missionId
+          ? mission
+          : { ...mission, reserved: true }
+      );
+      state.error = '';
+    });
+    builder.addCase(joinMission.rejected, (state, action) => {
       state.error = action.error;
     });
   },
