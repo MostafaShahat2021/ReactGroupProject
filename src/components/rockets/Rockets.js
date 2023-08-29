d264e94c632d9d9f7199cae6ef8928ba6551c016
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-29 19:49:10 +0300

    Add rockets styling

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './Rockets.css';
 
 function Rockets() {
   const { rockets } = useSelector((store) => store.rockets);
@@ -10,19 +11,17 @@ function Rockets() {
       </div>
       <div className="rocket-info">
         <h2>{rocket.name}</h2>
+        <div className="info-cntainer">
+          <span className="description">{rocket.description}</span>
+        </div>
+        <button className="rocket-btn" type="button">
+          Reserve Rocket
+        </button>
       </div>
-      <div className="info-cntainer">
-        <span className="description">{rocket.description}</span>
-      </div>
-      <button className="rocket-btn" type="button">Reserve Rocket</button>
     </div>
   ));
 
-  return (
-    <article>
-      {renderRockets}
-    </article>
-  );
+  return <article>{renderRockets}</article>;
 }
 
 export default Rockets;
