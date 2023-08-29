0bfc177d4c4374d8d5bfbd2e5900e2e27e771c31
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-29 11:30:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import NotFound from './components/NotFound';
 
 function App() {
@@ -7,9 +7,30 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<div><h1>Rockets</h1></div>} />
-        <Route path="/missions" element={<div><h1>Missions</h1></div>} />
-        <Route path="/profile" element={<div><h1>profile</h1></div>} />
+        <Route
+          path="/"
+          element={(
+            <div>
+              <h1>Rockets</h1>
+            </div>
+          )}
+        />
+        <Route
+          path="/missions"
+          element={(
+            <div>
+              <h1>Missions</h1>
+            </div>
+          )}
+        />
+        <Route
+          path="/profile"
+          element={(
+            <div>
+              <h1>profile</h1>
+            </div>
+          )}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
