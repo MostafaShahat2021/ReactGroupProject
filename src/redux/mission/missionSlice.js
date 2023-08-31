e20b2003d379c0cd772c869fd5915821a83a8569
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-31 13:52:45 +0100

    make changes

--- src/redux/mission/missionSlice.js
+++ src/redux/mission/missionSlice.js
@@ -12,13 +12,13 @@ export const fetchMission = createAsyncThunk(
   async () => {
     try {
       const response = await axios.get(
-        'https://api.spacexdata.com/v3/missions'
+        'https://api.spacexdata.com/v3/missions',
       );
       return response.data;
     } catch (error) {
       throw new Error(error.message);
     }
-  }
+  },
 );
 
 const missionSlice = createSlice({
